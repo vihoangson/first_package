f26c734dae8a8c070cb39d4411496a70b9527581
Vi Hoàng Sơn <vihoangson@gmail.com>
2021-04-28 17:03:10 +0700

    Update sayHello.php

--- src/HelloWorld/sayHello.php
+++ src/HelloWorld/sayHello.php
@@ -2,6 +2,6 @@
 namespace HelloWorld;
 class SayHello {
     public static function world() {
-        return 'Hello World, Composer! Keep common 1 ádasd23';
+        return 'Hello World, Composer! Keep common 1   123 123 ádasd23';
     }
 }
